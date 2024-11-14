ecc7f6b8dcd29d7d63af15d67054ae4bfa9ac1b0
ArcherWHChiu <149639047+ArcherWHChiu@users.noreply.github.com>
2024-11-15 00:01:01 +0800

    daily

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler
+{
+    
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Service to the container
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline
 
 app.Run();
